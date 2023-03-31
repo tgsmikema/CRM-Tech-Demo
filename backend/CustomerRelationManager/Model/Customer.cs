22a012e00c1e807af34388c8ea0d99c1eb0cfd7a
sma148 <sma148@aucklanduni.ac.nz>
2023-03-31 16:11:12 +1300

    set-up backend .NET WEB API Project, added model classes

---
+++ backend/CustomerRelationManager/Model/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerRelationManager.Model
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        public string? Description  { get; set; } // optional field
+        public string? PhoneNumber { get; set; } // optional field
+        [Required]
+        public DateTime CreatedDateAndTime { get; set; }
+        [Required]
+        public int CreatedByUserId { get; set; }
+
+
+    }
+}
---
+++ backend/CustomerRelationManager/Model/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerRelationManager.Model
+{
+    public class UserLogin
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string UserType { get; set; }
+
+    }
+}
