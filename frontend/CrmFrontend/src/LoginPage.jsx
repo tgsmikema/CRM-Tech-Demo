a9bbc29f846ae03b866c51911b9ab08d7349db13
sma148 <sma148@aucklanduni.ac.nz>
2023-04-01 00:55:21 +1300

    successfully connect the frontend to backend

--- frontend/CrmFrontend/src/LoginPage.jsx
+++ frontend/CrmFrontend/src/LoginPage.jsx
@@ -1,45 +1,55 @@
 import React, { useState } from 'react';
-import {
-  Button,
-  TextField,
-  Grid,
-  Typography,
-  Box,
-} from '@mui/material';
+import { Button, TextField, Grid, Typography, Box } from '@mui/material';
+import axios from "axios";
 
 const LoginPage = () => {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: handle form submission
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <Grid container spacing={2} justifyContent="center">
-      <Grid item xs={12} sm={8} md={6} lg={4}>
-        <Box mt={3} mb={3}>
-          <Typography variant="h3" align="center">
-            Login
-          </Typography>
-        </Box>
-        <form onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField type="text" required fullWidth label="Username" value={userName} onChange={(e) => setUserName(e.target.value)} />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField type="password" required fullWidth label="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </Grid>
-            <Grid item xs={12}>
-              <Button type="submit" variant="contained" color="primary" fullWidth>Login</Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Grid>
-    </Grid>
-  );
-};
+        axios({
+            method:'get',
+            url: `https://localhost:8080/api/login`,
+            auth: {
+                username: userName,
+                password: password
+            }
+        })
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
 
-export default LoginPage;
+
+        return (
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12} sm={8} md={6} lg={4}>
+                    <Box mt={3} mb={3}>
+                        <Typography variant="h3" align="center">
+                            Login
+                        </Typography>
+                    </Box>
+                    <form onSubmit={handleSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField type="text" required fullWidth label="Username" value={userName} onChange={(e) => setUserName(e.target.value)} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField type="password" required fullWidth label="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button type="submit" variant="contained" color="primary" fullWidth>Login</Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Grid>
+            </Grid>
+        );
+    };
+
+    export default LoginPage;
