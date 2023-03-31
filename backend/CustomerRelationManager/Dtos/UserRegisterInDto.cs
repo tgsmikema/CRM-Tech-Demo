e1f2238fdf4dc6da9541894c43d615e4518547b5
sma148 <sma148@aucklanduni.ac.nz>
2023-03-31 16:53:26 +1300

    added user register Dto object

---
+++ backend/CustomerRelationManager/Dtos/UserRegisterInDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomerRelationManager.Dtos
+{
+    public class UserRegisterInDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
