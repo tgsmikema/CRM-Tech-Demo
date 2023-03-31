d4f7fbabe77256ae47d59e0784cbea93e32fe109
sma148 <sma148@aucklanduni.ac.nz>
2023-03-31 17:01:19 +1300

    set up web api controller class, and a testing api endpoint

---
+++ backend/CustomerRelationManager/Controllers/CrmController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomerRelationManager.Data;
+using CustomerRelationManager.Dtos;
+using CustomerRelationManager.Model;
+
+namespace CustomerRelationManager.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CrmController : Controller
+    {
+        private readonly ICrmRepo _repository;
+
+        public CrmController(ICrmRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("testing")]
+        public ActionResult testingFunction()
+        {
+            return Ok("Testing Endpoint!");
+        }
+    }
+}
