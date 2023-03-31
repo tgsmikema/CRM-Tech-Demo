9bf21f9807b1f274d809c89431d5fa60f1f01adb
sma148 <sma148@aucklanduni.ac.nz>
2023-03-31 16:16:52 +1300

    create DBContext class

---
+++ backend/CustomerRelationManager/Data/CrmDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomerRelationManager.Model;
+using Task = CustomerRelationManager.Model.Task;
+
+namespace CustomerRelationManager.Data
+{
+    public class CrmDBContext : DbContext
+    {
+        public CrmDBContext(DbContextOptions<CrmDBContext> options) : base(options) { }
+        public DbSet<UserLogin> UserLogins { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
